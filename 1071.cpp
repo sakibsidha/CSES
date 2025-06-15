d0a73e1c92face5a267852324c9fdba58636cadf
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-06-15 13:34:12 +0600

    Create 1071.cpp

---
+++ 1071.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+  ll row, col; cin >> row >> col;
+  ll lvl = max(row, col), n = lvl - 1, nth_term = 1 + (n - 1) * 2;
+  ll sum = n * (1 + nth_term) / 2, begin = sum + 1, end = begin + n * 2;
+  if (lvl == row) {
+    if (lvl & 1) cout << begin + col - 1 << "\n";
+    else cout << end - col + 1 << "\n";
+  } else {
+    if (lvl % 2 == 0) cout << begin + row - 1 << "\n";
+    else cout << end - row + 1 << "\n";
+  }
+}
+
+int32_t main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int tc; cin >> tc;
+  while (tc--)
+    solve();
+
+  return 0;
+}
