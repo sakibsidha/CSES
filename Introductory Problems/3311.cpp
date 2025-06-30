2c50642de51f2376d641d8ec7e8c59f2f73d60c1
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-06-30 17:16:26 +0600

    Update 3311.cpp

--- Introductory Problems/3311.cpp
+++ Introductory Problems/3311.cpp
@@ -19,13 +19,13 @@ void solve() {
   }
 }  
 
-int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	// int tc; cin >> tc;
-	// while(tc--)
-		solve();
-
-	return 0;
+  int tc = 1;
+  // cin >> tc;
+  while (tc--) {
+    solve();
+  }
 }
