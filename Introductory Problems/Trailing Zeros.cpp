a793647c9116b4bf3c41821da81e13dbc68e8a3d
Kh Sadman Sakib <119437906+sakibsidha@users.noreply.github.com>
2025-07-09 15:00:04 +0600

    Update and rename 1618.cpp to Trailing Zeros.cpp

---
+++ Introductory Problems/Trailing Zeros.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+  ll n; cin >> n;
+  ll ans = 0;
+  for (ll i = 5; i <= n; i *= 5) {
+    ans += (n / i);
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int tc = 1;
+  // cin >> tc;
+  for (int i = 0; i < tc; ++i) {
+    solve();
+  }
+}
